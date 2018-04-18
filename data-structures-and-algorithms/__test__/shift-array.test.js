0800a70c41695e1c138de34240d83c6c83afe36f
SethDonohue <donohue.seth@gmail.com>
2018-04-18 13:55:18 -0700

    all good for shift array tests and setup

--- data-structures-and-algorithms/__test__/shift-array.test.js
+++ data-structures-and-algorithms/__test__/shift-array.test.js
@@ -1,18 +1,21 @@
-const shiftArray = require('../shift-array.js');
+const shiftArray = require('../shift-array');
+// import insertShiftArray from '../shift-array';
 
-const testArr = [0,1,2,3,4,5,6,7,8];
-const testArrAns = [0,1,2,3,4,100,5,6,7,8];
-const testArrTwo = [0,1,2,3,4,5,6,7,8,9];
-const testArrTwoAns = [0,1,2,3,4,100,5,6,7,8,9];
+console.log(shiftArray.insertShiftArray);
+
+const testArr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+const testArrAns = [0, 1, 2, 3, 4, 100, 5, 6, 7, 8];
+const testArrTwo = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const testArrTwoAns = [0, 1, 2, 3, 4, 100, 5, 6, 7, 8, 9];
 
 const value = 100;
 
 describe('insertShiftArray', () => {
-  test('Function should return an array with the value inserted in the middle of an odd length array, where the inserted element will be at the midpoint plus +1 since the resulting length will be even.', ()=>{
-    expect(insertShiftArray(testArr)).toEqual(testArrAns);
-  })
+  test('Function should return an array with the value inserted in the middle of an odd length array, where the inserted element will be at the midpoint plus +1 since the resulting length will be even.', () => {
+    expect(shiftArray.insertShiftArray(testArr, value)).toEqual(testArrAns);
+  });
 
-  test('Function should return an array with the value inserted in the middle of an even length array.', ()=>{
-    expect(insertShiftArray(testArr)).toEqual(testArrAns);
-  })
-})
+  test('Function should return an array with the value inserted in the middle of an even length array.', () => {
+    expect(shiftArray.insertShiftArray(testArrTwo, value)).toEqual(testArrTwoAns);
+  });
+});
--- data-structures-and-algorithms/shift-array.js
+++ data-structures-and-algorithms/shift-array.js
@@ -1,19 +1,23 @@
-const insertShiftArray = (arr, value) => {
+const shiftArray = module.exports = {};
 
-  const midPointIndex = Math.floor(arr.length/2);
-  let result = [];
 
-  for(let i = 0; i < arr.length; i++){
-    if(i < midPointIndex){
+shiftArray.insertShiftArray = (arr, value) => {
+  const midPointIndex = Math.ceil(arr.length / 2);
+  const result = [];
+
+  for (let i = 0; i < arr.length + 1; i++) {
+    if (i < midPointIndex) {
       result[i] = arr[i];
-    } else if( i === midPointIndex){
+    } else if (i === midPointIndex) {
       result[i] = value;
     } else {
-      result[i] = arr[i-1];
+      result[i] = arr[i - 1];
     }
   }
   return result;
-}
+};
 
-console.log(insertShiftArray([0,1,2,3,4,5,6], 100));
-console.log(insertShiftArray([0,1,2,3,4,5], 100));
+// console.log(insertShiftArray([0, 1, 2, 3, 4, 5, 6], 100));
+// console.log(insertShiftArray([0, 1, 2, 3, 4, 5], 100));
+
+// exports.insertShiftArray = insertShiftArray;
